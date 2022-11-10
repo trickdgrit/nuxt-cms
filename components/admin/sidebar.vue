dacf2ec882be3b20f08cb4f0746a7322632322f0
trickdgrit <t3ck17@gmail.com>
2022-11-11 05:47:48 +0800

    tambahkan halaman admin

---
+++ components/admin/sidebar.vue
@@ -0,0 +1,79 @@
+<template>
+  <!-- Sidebar Menu -->
+  <nav class="mt-2">
+    <ul
+      class="nav nav-pills nav-sidebar flex-column"
+      data-widget="treeview"
+      role="menu"
+      data-accordion="false"
+    >
+      <li class="nav-item">
+        <nuxt-link :to="{ name: 'admin-dashboard' }" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>Dashboard</p>
+        </nuxt-link>
+      </li>
+
+      <li class="nav-header">
+        <h6>CONTENT</h6>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-tags"></i>
+          <p>Tags</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-folder"></i>
+          <p>Categories</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-book-open"></i>
+          <p>Posts</p>
+        </a>
+      </li>
+
+      <li class="nav-header">
+        <h6>SETTINGS</h6>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-clone"></i>
+          <p>Menus</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-image"></i>
+          <p>Sliders</p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-users"></i>
+          <p>Users</p>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+a.nuxt-link-active {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+</style>
