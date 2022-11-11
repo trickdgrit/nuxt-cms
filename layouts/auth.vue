a91118630711de7b545a7918b298fbcc1fefbe21
trickdgrit <t3ck17@gmail.com>
2022-11-11 08:19:37 +0800

    tambah file auth

---
+++ layouts/auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hold-transition login-page">
+    <div class="login-box">
+      <!-- /.login-logo -->
+      <Nuxt />
+      <!-- /.card -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //middleware auth
+  middleware: "auth",
+};
+</script>
+
+<style>
+body {
+  font-family: "Quicksand", sans-serif;
+}
+
+.login-page {
+  background-color: #ccdcec !important;
+}
+
+.card-info.card-outline {
+  border-top: 3px solid #186399;
+}
+
+.btn-info {
+  color: #fff;
+  background-color: #186399;
+  border-color: #186399;
+  box-shadow: none;
+}
+
+.btn-info:hover {
+  color: #fff;
+  background-color: #12466b;
+  border-color: #12466b;
+  box-shadow: none;
+}
+</style>
